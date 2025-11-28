aa3ed23fbb2e76a6a383e53306dcfc1e8264c8f5
kuriko16 <msimsek2@student.tgm.ac.at>
2025-11-28 09:41:45 +0100

    Dummy-Daten

---
+++ src/myapp/gui/dummy_gui_data.py
@@ -0,0 +1,14 @@
+GUI_DUMMY_HABITS = [
+    {
+        "name": "Workout",
+        "description": "30 Minuten Training",
+        "frequency": "daily",
+        "is_done_today": False
+    },
+    {
+        "name": "Meditation",
+        "description": "10 Minuten entspannen",
+        "frequency": "daily",
+        "is_done_today": True
+    }
+]
